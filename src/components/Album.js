483a744f244fc2332cc6d48b4cc595f18aeae029
Stephanie <stephaniesantamaria58@yahoo.com>
2019-03-27 20:05:27 -0700

    almost completed assignment

--- src/components/Album.js
+++ src/components/Album.js
@@ -68,6 +68,14 @@ class Album extends Component {
        this.play();
     }
 
+    handleNextClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.max(currentIndex + 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+    }
+
      render() {
        return (
        <section className="album">
@@ -102,6 +110,7 @@ class Album extends Component {
            currentSong={this.state.currentSong}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
+           handleNextClick={() => this.handleNextClick()}
          />
        </section>
      )
